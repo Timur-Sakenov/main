92a9dc9b6a9fea1a2cdd305d4d2d788c3a4dac44
Tima <tsakienov@mail.ru>
2021-01-04 20:20:10 +0600

    16

--- main.go
+++ main.go
@@ -306,5 +306,30 @@ w.Write([]byte("Create a new snippet..."))
 
  */
 
+/* 13
+
+package main2
+import (
+"fmt"
+"net/http"
+"runtime/debug"
+)
+
+func (app *application) serverError(w http.ResponseWriter, err error) {
+trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+app.errorLog.Println(trace)
+http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *application) clientError(w http.ResponseWriter, status int) {
+http.Error(w, http.StatusText(status), status)
+}
+
+func (app *application) notFound(w http.ResponseWriter) {
+app.clientError(w, http.StatusNotFound)
+}
+
+
+ */
 
 
