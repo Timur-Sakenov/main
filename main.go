0c2ed8461c5b141f261c0c1e0714fe6256fd61bf
Tima <tsakienov@mail.ru>
2021-01-04 20:14:11 +0600

    11

--- main.go
+++ main.go
@@ -168,7 +168,31 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 */
 
+/* 8
+package main2
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	log.Fatal(err)
+}
 
 
+ */
 
 
